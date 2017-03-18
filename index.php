723f0b8c0ebed632cdf337aaf87bc20790041ad2
rfigueredo5970 <refo44@gmail.com>
2017-03-18 11:00:08 -0400

    Commit inicial

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Carrito de compras</title>
+	<link rel="stylesheet" href="css/estilos.css">
+	<script type="text/javascript" src="js/scripts.js"></script>
+</head>
+<body>
+
+	<header>
+		<h1>Carrito De Compras</h1>
+		<a href="carritodecompras.php" title="ver carrito de compras">
+			<img src="imagenes/carrito.png" alt="imagen carrito de compras">
+		</a>
+	</header>
+
+	<section>
+		<div class="producto">
+			
+		</div>
+	</section>
+
+
+	
+</body>
+</html>
