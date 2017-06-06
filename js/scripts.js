9bb4419f8688161542ad05ebf82047c65c2cbe3f
Rafael Figueredo <refo44@gmail.com>
2017-06-06 11:23:08 -0400

    Add delete products for shopping cart

--- js/scripts.js
+++ js/scripts.js
@@ -25,6 +25,25 @@ var inicio = function () {
 			}
 		}
 	});
+
+	$(".eliminar").click(function(e) {
+		e.preventDefault();
+
+		var id = $(this).attr('data-id');
+		$(this).parentsUntil(".productos").remove();
+
+		$.post("./js/eliminar.php",{
+
+			Id:id
+
+		},function (a) {
+			
+			if (a == '0') {
+				location.href='./carritodecompras.php';
+			}
+		}); 
+
+	});
 }
 
 $(document).ready(function() {
